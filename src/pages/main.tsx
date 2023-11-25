d1cf3f7f569bfa26811564e68984a02b703c87c8
Gleb <gleborlov20022002@gmail.com>
2023-11-25 15:10:53 +0300

    add offers empty component

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,6 +2,8 @@ import CitiesComponent from '../components/cities';
 import HeaderComponent from '../components/header';
 import LoadingComponent from '../components/loading';
 import OffersListComponent from '../components/offers-list';
+import EmptyOffersComponent from '../components/empty-offers';
+import classNames from 'classnames';
 import { useAppSelector } from '../hooks';
 import { sortingCards } from '../util';
 import { RequestStatus } from '../const';
@@ -10,7 +12,8 @@ export default function MainPage(): JSX.Element {
   const selectedCity = useAppSelector((state) => state.offers.city);
   const cardsSortingType = useAppSelector((state) => state.offers.sortingType);
   const allOffers = useAppSelector((state) => state.offers.offers);
-  const fecthingOffersStatus = useAppSelector((state) => state.loading.offersFetchingStatus);
+  const isOffersLoaded = useAppSelector((state) => state.loading.offersFetchingStatus)
+    === RequestStatus.Success;
 
   const filteredOffersByCity = allOffers.filter((offer) => offer.city.name === selectedCity);
   const sortedOffers = sortingCards[cardsSortingType](filteredOffersByCity);
@@ -19,14 +22,19 @@ export default function MainPage(): JSX.Element {
     <div className="page page--gray page--main">
       <HeaderComponent />
 
-      <main className="page__main page__main--index">
+      <main className={classNames(
+        'page__main page__main--index',
+        { 'page__main--index-empty': isOffersLoaded && !filteredOffersByCity.length }
+      )}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <CitiesComponent />
         </div>
         <div className="cities">
-          {fecthingOffersStatus !== RequestStatus.Success && <LoadingComponent />}
-          {fecthingOffersStatus === RequestStatus.Success && <OffersListComponent offers={sortedOffers} selectedCity={selectedCity} />}
+          {!isOffersLoaded && <LoadingComponent />}
+          {(isOffersLoaded && !filteredOffersByCity.length) && <EmptyOffersComponent />}
+          {isOffersLoaded && <OffersListComponent offers={sortedOffers} selectedCity={selectedCity} />}
         </div>
       </main>
     </div>
