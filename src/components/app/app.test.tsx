b61f436aa0535430d259a237bafa6b77f7da45ef
Gleb <gleborlov20022002@gmail.com>
2023-12-03 18:39:53 +0300

    add folder for app

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryHistory, createMemoryHistory } from 'history';
-import { AppRoute } from '../const';
-import { withStore } from '../utils/mock-component';
+import { AppRoute } from '../../const';
+import { withStore } from '../../utils/mock-component';
 import App from './app';
-import { makeFakeStore } from '../utils/mocks';
+import { makeFakeStore } from '../../utils/mocks';
 
 describe('Application routing', () => {
   let mockHistory: MemoryHistory;
